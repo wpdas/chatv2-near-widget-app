c3190ec53c84cc6c28b65211b6f11556767d1d3d
Wenderson Pires <wendersonpdas@gmail.com>
2023-03-20 02:03:57 -0300

    added Fallback

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,7 +4,12 @@ import { NavigationProps } from "./NavigationProps";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 
-const { Navigator, Screen } = createStackNavigator<NavigationProps>();
+// Optional Fallback Loading component to show while the connection is being established
+const FallbackLoadingComponent = () => <p>Loading...</p>;
+
+const { Navigator, Screen } = createStackNavigator<NavigationProps>(
+  <FallbackLoadingComponent />
+);
 
 const Routes: React.FC = () => {
   return (
