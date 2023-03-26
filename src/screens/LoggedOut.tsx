44adc3b0298cf6bad5707425ac4935cdfb2731ab
Wenderson Pires <wendersonpdas@gmail.com>
2023-03-26 20:46:44 -0300

    New UI, List of created rooms, url detector, image viewer, new resources

--- src/screens/LoggedOut.tsx
+++ src/screens/LoggedOut.tsx
@@ -1,20 +1,17 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { LoggedOutScreenProps } from "../routes/NavigationProps";
 import Container from "../components/Container";
-import Content from "../components/Content";
-import Header from "../components/Header";
 import { ChatIcon } from "@chakra-ui/icons";
 
 const LoggedOut: React.FC<LoggedOutScreenProps> = () => {
   return (
     <Container>
-      <Header hideButtons />
-      <Content>
+      <Box w="100%" display="flex" flexDirection="column" alignItems="center">
         <ChatIcon boxSize={12} mt={16} color="teal" />
         <Text size="xs" mt={4} color="gray.700" maxW="sm" textAlign="center">
           You need to sign in to use this chat!
         </Text>
-      </Content>
+      </Box>
     </Container>
   );
 };
