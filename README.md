91b4fce216d5dcb80d7a174bf57d23b0b4e84334
Wenderson Pires <wendersonpdas@gmail.com>
2023-04-05 02:39:19 -0300

    using `near-social-local-viewer`, included the `widget/ChatV2.jsx` file

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # ChatV2 Widget App
 
-This is an application running on Near Social using the NEAR blockchain network. This app was built as part of testing the [NEAR Social Bridge](https://github.com/wpdas/near-social-bridge) library.
+This is an application running on Near Social using the NEAR blockchain network.
 
-You can access it here: [**ChatV2**](https://near.social/#/wendersonpires.near/widget/ChatV2)
+[**Live App**](https://near.social/#/wendersonpires.near/widget/ChatV2)
+
+## Setup
+
+Install all the dependencies:
+
+```sh
+yarn install
+```
+
+Run app:
+
+```sh
+yarn start:app
+```
+
+Run viewer
+
+```sh
+yarn start:widget
+```
+
+Now, you can see your changes in real time. Both in React App and Widget.
+
+## Libraries
+
+[**NEAR Social Bridge:**](https://github.com/wpdas/near-social-bridge) A library that allows you to create a common application using ReactJS and inject it in a controlled way into a Widget on Near Social. Therefore, the Widget talks to the React application and vice versa, making it possible to consume Discovery API resources within the React application.
+
+[**NEAR Social Local Viewer:**](https://github.com/wpdas/near-social-local-viewer) A CLI tool that allows you to run and test your Near Social Widgets locally using just your preferred code editor and your default browser.
---
+++ widget/ChatV2.jsx
@@ -0,0 +1,208 @@
+/**
+ * This Widget was built using NEAR Social Bridge library.
+ * Visit https://github.com/wpdas/near-social-bridge to get to know more.
+ */
+
+/**
+ * App index key to store things (only rooms as this app is re fetching messages from ChatV1)
+ * It should use a non "-dev" key for V3. This is being used because rooms were already created
+ */
+const APP_INDEX_KEY = "widget-chatv2-dev";
+
+/**
+ * App setup
+ */
+// const externalAppUrl = "https://near-test-app.web.app/";
+const externalAppUrl = "http://localhost:3000";
+
+const path = props.path;
+const initialViewHeight = 740;
+const initialPayload = {
+  mainDomain: "https://alpha.near.org",
+  room: props.room, // starts with this room
+};
+
+/**
+ * Request Handlers.
+ */
+const requestHandler = (request, response, Utils) => {
+  switch (request.type) {
+    case "get-room-data":
+      getRoomDataHandler(request, response, Utils);
+      break;
+    case "send-message":
+      sendMessageHandler(request, response);
+      break;
+    case "register-new-room":
+      registerNewRoomHandler(request, response, Utils);
+      break;
+    case "get-rooms-list":
+      getRoomsListHandler(request, response, Utils);
+      break;
+    case "set-clipboard-text":
+      setClipboardTextHandler(request, response);
+      break;
+  }
+};
+
+const getRoomDataHandler = (request, response, Utils) => {
+  const { payload } = request;
+
+  if (!payload.roomId) {
+    response(request).send({ error: "roomId prop must be provided" });
+    return;
+  }
+
+  Utils.promisify(
+    () =>
+      Social.index(payload.roomId, "data", {
+        subscribe: true,
+        limit: 100,
+        order: "desc",
+      }),
+    (roomData) => {
+      const roomExists = roomData && roomData.length > 0;
+      if (!roomExists) {
+        response(request).send({ error: "room not found" });
+        return;
+      }
+      response(request).send({ messages: roomData });
+    },
+    (err) => {
+      response(request).send({ error: "internal error" });
+    }
+  );
+};
+
+const sendMessageHandler = (request, response) => {
+  const { payload } = request;
+  if (payload.roomId && payload.message) {
+    // Store message.
+    Social.set(
+      {
+        index: {
+          [payload.roomId]: JSON.stringify(
+            {
+              key: "data",
+              value: payload.message,
+            },
+            undefined,
+            0
+          ),
+        },
+      },
+      {
+        force: true,
+        onCommit: () => {
+          response(request).send({});
+        },
+        onCancel: () => {
+          response(request).send({ error: "the action was canceled" });
+        },
+      }
+    );
+    return;
+  }
+
+  // Error
+  response(request).send({
+    error: "you must provide the roomId and a message prop",
+  });
+};
+
+const registerNewRoomHandler = (request, response, Utils) => {
+  const { roomId } = request.payload;
+  if (!roomId) {
+    response(request).send({ error: "you must provide the roomId prop" });
+    return;
+  }
+
+  Utils.promisify(
+    () => fetchRooms(),
+    (rooms) => {
+      if (rooms.includes(roomId)) {
+        response(request).send({ roomsList: rooms });
+        return;
+      }
+
+      // Update the rooms list
+      const updatedRoomsList = [...rooms, roomId];
+      // Register it on chain
+      Social.set(
+        {
+          index: {
+            [APP_INDEX_KEY]: JSON.stringify(
+              {
+                key: "room",
+                value: roomId,
+              },
+              undefined,
+              0
+            ),
+          },
+        },
+        {
+          force: true,
+          onCommit: () => {
+            response(request).send({ roomsList: updatedRoomsList });
+          },
+          onCancel: () => {
+            response(request).send({ error: "the action was canceled" });
+          },
+        }
+      );
+    },
+    () => {
+      response(request).send({ error: "unknown error" });
+    }
+  );
+};
+
+const getRoomsListHandler = (request, response, Utils) => {
+  Utils.promisify(
+    () => fetchRooms(),
+    (rooms) => {
+      // Send the rooms list
+      response(request).send({ roomsList: rooms });
+    },
+    () => {
+      response(request).send({ error: "rooms list is not set", roomsList: [] });
+    }
+  );
+};
+
+const setClipboardTextHandler = (request, response) => {
+  if (request.payload.text) {
+    // limited by VM (not working for now) - wip
+    clipboard.writeText(request.payload.text);
+  }
+  response(request).send({});
+};
+
+// Helpers
+const fetchRooms = () => {
+  const data = Social.index(APP_INDEX_KEY, "room", {
+    subscribe: true,
+    limit: 1000,
+    order: "desc",
+  });
+
+  if (!data) return null;
+
+  const sorted = data.sort((m1, m2) => m2.blockHeight - m1.blockHeight);
+  return sorted.map((roomData) => roomData.value); // ["room-name"]
+};
+// Helpers END
+
+return (
+  <Widget
+    src={"wendersonpires.near/widget/NearSocialBridgeCore"}
+    props={{
+      externalAppUrl,
+      path,
+      initialViewHeight,
+      initialPayload,
+      requestHandler,
+    }}
+  />
+);
