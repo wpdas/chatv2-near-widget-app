3dbac782243a8bca3eb3168efb90a3100133831a
Wenderson Pires <wendersonpdas@gmail.com>
2023-04-11 00:33:48 -0300

    CopyToClipboardModal widget update

--- widget/CopyToClipboardModal.jsx
+++ widget/CopyToClipboardModal.jsx
@@ -84,36 +84,3 @@ return (
     </ModalContent>
   </Modal>
 );
-
-return (
-  <OverlayTrigger
-    placement="auto"
-    overlay={
-      <Tooltip>{state.copied ? "Copied!" : "Copy to clipboard"}</Tooltip>
-    }
-  >
-    <button
-      className={props.className ?? "btn btn-outline-success border-0"}
-      onClick={() => {
-        clipboard.writeText(props.text).then(() => {
-          State.update({ copied: true });
-          if (props.onCopy) {
-            props.onCopy(props.text);
-          }
-        });
-      }}
-    >
-      {state.copied ? (
-        <>
-          {props.copiedIcon ?? <i className="bi bi-check-lg success" />}{" "}
-          {props.copiedLabel ?? props.label}
-        </>
-      ) : (
-        <>
-          {props.clipboardIcon ?? <i className="bi bi-clipboard" />}{" "}
-          {props.label}
-        </>
-      )}
-    </button>
-  </OverlayTrigger>
-);
